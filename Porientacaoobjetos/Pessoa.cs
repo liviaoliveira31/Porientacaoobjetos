12b931e609781ab2719daf9bcad0dd11aefdf7f9
Livia Oliveira <livia.oliveira3104@outlook.com>
2022-08-24 13:43:06 -0300

    implementação completa e teste de manipulação de objetos

---
+++ Porientacaoobjetos/Pessoa.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porientacaoobjetos
+{
+    internal class Pessoa
+    {
+        String Nome;
+        DateTime DataNasc;
+        String Cpf;
+        long Telefone;
+
+        public Pessoa()//objeto construtor
+        {
+
+        }
+
+        public Pessoa(string nome, DateTime dataNasc, string cpf, long telefone)
+        {
+            this.Nome = nome;
+            this.DataNasc = dataNasc;
+            this.Cpf = cpf;
+            this.Telefone = telefone;
+
+        }
+
+        public void setNome()//tipo de acesso/ retorno do metodo/ nome
+        {
+            Console.WriteLine("Informe o nome:");
+            this.Nome = Console.ReadLine();
+        }
+        public void getNome()
+        {
+            Console.WriteLine("Nome: " + this.Nome);
+        }
+
+        public void setDataNasc()
+        {
+            Console.WriteLine("Informe a data de nascimento: ");
+            this.DataNasc = DateTime.Parse(Console.ReadLine());
+        }
+        public void getDataNasc()
+        {
+            Console.WriteLine("Data de nascimeento: " + this.DataNasc);
+        }
+
+        public void setCpf()
+        {
+            Console.WriteLine("Informe o cpf: ");
+            this.Cpf = Console.ReadLine();
+
+        }
+        public void getCpf()
+        {
+            Console.WriteLine("Cpf: " + this.Cpf);
+        }
+
+
+        public void setTelefone()
+        {
+            Console.WriteLine("informe o telefone: ");
+            this.Telefone = long.Parse(Console.ReadLine());
+        }
+        public void getTelefone()
+        {
+            Console.WriteLine("Telefone: " + this.Telefone);
+        }
+
+        public string ImprimirCadastro() //metodo = função
+        {
+            return "\nNome: " + this.Nome + "\nData de nascimento: " + this.DataNasc.ToLongDateString() +
+                   "\nCPF: " + this.Cpf + "\nTelefone: " + this.Telefone;
+        }
+
+        public void CadastrarPessoa()
+        {
+            setNome();
+            setDataNasc();
+            setCpf();
+            setTelefone();
+        }
+
+    }
+}
--- Porientacaoobjetos/Program.cs
+++ Porientacaoobjetos/Program.cs
@@ -6,7 +6,25 @@ namespace Porientacaoobjetos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoa pessoa1 = new Pessoa();
+
+            pessoa1.CadastrarPessoa();
+
+            Console.WriteLine(pessoa1.ImprimirCadastro());
+
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Data de nascimento: ");
+            DateTime data = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("CPF: ");
+            string cpf = Console.ReadLine();
+            Console.WriteLine("Telefone: ");
+            long telefone = long.Parse(Console.ReadLine());
+
+            Pessoa pessoa2 = new Pessoa(nome, data, cpf, telefone);
+            Console.WriteLine(pessoa2.ImprimirCadastro());
+
+
         }
     }
 }
